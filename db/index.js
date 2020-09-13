21c459cb5d11b96839ed0ef2f7d7f30cd0d6c07f
marioviana07 <mario_viana_2005@yahoo.com>
2020-09-12 21:50:43 -0700

    Finish the regular program I need to work in bonus

--- db/index.js
+++ db/index.js
@@ -22,7 +22,7 @@ class EmployeeDB {
 
     getEmployees() {
         return this.connection.promise().query(
-            `SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary
+            `SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary,
             CONCAT(manager.first_name,'', manager.last_name) AS manager
             FROM employee
             LEFT JOIN role ON employee.role_id = role.id
@@ -35,7 +35,35 @@ class EmployeeDB {
         return this.connection.promise().query(
             `SELECT * FROM employee`
         );
-    }
+    };
+
+    addDepartment(name) {
+        return this.connection.promise().query(
+            `INSERT INTO department(name) 
+        VALUES(?)`, name
+        )
+    };
+
+    addRole(title, salary, department_id) {
+        return this.connection.promise().query(
+            `INSERT INTO role(title, salary, department_id)
+        VALUES(?,?,?)`, [title, salary, department_id]
+        )
+    };
+
+    addEmployee(first_name, last_name, role_id, manager_id) {
+        return this.connection.promise().query(
+            `INSERT INTO employee(first_name, last_name, role_id, manager_id)
+        VALUES(?,?,?)`, [first_name, last_name, role_id, manager_id]
+        )
+    };
+
+    updateRole(employee_id, role_id) {
+        return this.connection.promise().query(
+            `UPDATE employee SET role_id = ? WHERE id = ?`, [role_id, employee_id]
+        )
+    };
+
 };
 
 module.exports = new EmployeeDB(connection)
--- db/schema.sql
+++ db/schema.sql
@@ -26,8 +26,6 @@ CREATE TABLE employee (
   INDEX rol_ind (role_id),
   INDEX man_ind (manager_id),
   PRIMARY KEY (id)
-  CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role (id) ON DELETE CASCADE,
-  CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee (id) ON DELETE CASCADE
 );
 
 
